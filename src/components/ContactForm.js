25b606e058341c9af9c671bf364b930b2606ec8d
Lakshmigithub-ui <seetaraman176@gmail.com>
2024-07-11 13:56:27 +0530

    editing

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,54 +1,55 @@
-import React, { useState } from 'react';
-import Form from './Form.css'
+import React, { useState } from "react";
+import Form from "./Form.css";
 
-const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    contactNumber: '',
-    dob: '',
-    gender: '',
-  });
-
+function ContactForm() {
 
   return (
-    <form  className='style'>
-      <br />  <br />
-      <label>
-        Name:
-        <input  type="text" name="name" placeholder="Type your name"/>
-      </label>
-
-      <br /> <br />
-
-      <label>
-        Contact Number:
-        <input type="text" name="contactNumber" placeholder="Contact number" />
-      </label>
-      <br /> <br />
-
-
-      <label>
-        Date of Birth:
-        <input type="date" name="dob"  />
-      </label>
-      <br /> <br />
-
-
-      <label>
-        Gender:
-        <select name="gender" >
-          <option value="">Select</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-      </label>
-      <br />
-      <br />
-
-      <button className='style1' type="submit">Submit</button>
-    </form>
+    <>
+      <form className="style">
+        <br /> <br />
+        <label>
+          Name:
+          <input type="text" name="name" placeholder="Type your name" />
+        </label>
+        <br /> <br />
+        <label>
+          Contact Number:
+          <input
+            type="text"
+            name="contactNumber"
+            placeholder="Contact number"
+          />
+        </label>
+        <br /> <br />
+        <label>
+          Date of Birth:
+          <input type="date" name="dob" />
+        </label>
+        <br /> <br />
+        <label>
+          Gender:
+          <select name="gender">
+            <option value="">Select</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <br />
+        <br />
+        <input
+          type="button"
+          value="choose file"
+          placeholder="choose file"
+        ></input>
+        <input type="button" value="Upload file"></input>
+        <br></br>
+        <button className="style1" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
   );
-};
+}
 
 export default ContactForm;
